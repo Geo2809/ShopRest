691f906df3c20ce5151506568c281460b8b51e97
George <george.9409@yahoo.ro>
2022-07-02 19:56:42 +0300

    Add FBV product_list/detail, collection_list/detail

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list, name='product-list'),
-    path('products/<int:id>/', views.product_detail, name='product-detail')
+    path('products/<int:id>/', views.product_detail, name='product-detail'),
+    path('collections/', views.collection_list, name='collection-list'),
+    path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
 ]
