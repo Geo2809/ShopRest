f210649d4b16490e71a1d5d117fcfdb4f47869eb
George <george.9409@yahoo.ro>
2023-03-07 11:37:14 +0200

    Add the authentication endpoints using JWT

--- ShopRest/settings.py
+++ ShopRest/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     
     "debug_toolbar",
     'rest_framework',
+    'djoser',
 
     'shop',
     'core'
@@ -144,3 +145,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
